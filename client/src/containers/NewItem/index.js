57d3ade7e4acc663634f5def07cbddbee6bf6202
Ev Bogdanov <evbogdanov.com@gmail.com>
2019-02-09 22:32:26 +0300

    Rewrite mutation using Apollo client

--- client/src/containers/NewItem/index.js
+++ client/src/containers/NewItem/index.js
@@ -1,8 +1,35 @@
 import React, { Component } from 'react';
+import { Link } from 'react-router-dom';
+import gql from 'graphql-tag';
+import { Mutation } from 'react-apollo';
 
 import Input from '../../components/Input/';
 
-class NewItem extends Component {
+const CREATE_ITEM = gql`
+  mutation CreateItem(
+    $name: String!
+    $price: Int!
+    $quantity: Int!
+    $purchased: Boolean!
+  ) {
+    createItem(
+      input: {
+        name: $name
+        price: $price
+        quantity: $quantity
+        purchased: $purchased
+      }
+    ) {
+      id
+      name
+      price
+      quantity
+      purchased
+    }
+  }
+`;
+
+class Form extends Component {
   state = {
     name: '',
     price: 0,
@@ -15,85 +42,92 @@ class NewItem extends Component {
     if (key === 'purchased') {
       this.setState(prevState => ({ purchased: !prevState.purchased }));
     } else {
-      this.setState({ [key]: event.target.value });
+      let value = event.target.value;
+      if (key === 'price' || key === 'quantity') value = parseInt(value, 10);
+      this.setState({ [key]: value });
     }
   };
 
   handleSubmit = event => {
     event.preventDefault();
-    const { name, price, quantity, purchased } = this.state;
-    const query = `
-mutation {
-  createItem(input: {name: "${name}", price: ${price}, quantity: ${quantity}, purchased: ${purchased}}) {
-    id
-    name
-    price
-    quantity
-    purchased
-  }
-}
-`;
-    fetch('/graphql', {
-      method: 'POST',
-      body: JSON.stringify({ query }),
-      headers: {
-        'Content-Type': 'application/json'
-      }
-    })
-      .then(res => res.json())
-      .then(data => console.log(data))
-      .catch(err => console.error(err));
+    this.props.createItem({
+      variables: { ...this.state }
+    });
   };
 
   render() {
     return (
-      <>
+      <form onSubmit={this.handleSubmit}>
         <h1>Create new item</h1>
-        <form onSubmit={this.handleSubmit}>
-          <p>
-            <Input
-              name="name"
-              placeholder="Name"
-              type="text"
-              value={this.state.name}
+        <p>
+          <Input
+            name="name"
+            placeholder="Name"
+            type="text"
+            value={this.state.name}
+            onChange={this.handleChange}
+          />
+        </p>
+        <p>
+          <Input
+            name="price"
+            placeholder="Price"
+            type="number"
+            value={this.state.price}
+            onChange={this.handleChange}
+          />
+        </p>
+        <p>
+          <Input
+            name="quantity"
+            placeholder="Quantity"
+            type="number"
+            value={this.state.quantity}
+            onChange={this.handleChange}
+          />
+        </p>
+        <p>
+          <label htmlFor="purchased">
+            Purchased:{' '}
+            <input
+              name="purchased"
+              id="purchased"
+              type="checkbox"
+              checked={this.state.purchased}
               onChange={this.handleChange}
             />
-          </p>
-          <p>
-            <Input
-              name="price"
-              placeholder="Price"
-              type="number"
-              value={this.state.price}
-              onChange={this.handleChange}
-            />
-          </p>
-          <p>
-            <Input
-              name="quantity"
-              placeholder="Quantity"
-              type="number"
-              value={this.state.quantity}
-              onChange={this.handleChange}
-            />
-          </p>
-          <p>
-            <label htmlFor="purchased">
-              Purchased:{' '}
-              <input
-                name="purchased"
-                id="purchased"
-                type="checkbox"
-                checked={this.state.purchased}
-                onChange={this.handleChange}
-              />
-            </label>
-          </p>
-          <button type="submit">Create</button>
-        </form>
-      </>
+          </label>
+        </p>
+        <button type="submit">Create</button>
+      </form>
     );
   }
 }
+
+const Success = ({ id, name, price, quantity, purchased }) => (
+  <>
+    <h1>Item created</h1>
+    <h4>
+      <Link to={`/item/${id}`}>Go to item</Link>
+    </h4>
+    <ul>
+      <li>Name: {name}</li>
+      <li>Price: {price}</li>
+      <li>Quantity: {quantity}</li>
+      <li>Purchased: {purchased ? 'YES' : 'NO'}</li>
+    </ul>
+  </>
+);
+
+const NewItem = () => (
+  <Mutation mutation={CREATE_ITEM}>
+    {(createItem, { loading, error, data }) => {
+      if (loading) return <p>Loading...</p>;
+      if (error) return <p>Error</p>;
+      if (data) return <Success {...data.createItem} />;
+      return <Form createItem={createItem} />;
+    }}
+  </Mutation>
+);
 
 export default NewItem;
